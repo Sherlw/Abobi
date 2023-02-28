5125f10feda8d8286f2db7be75665efa9ba47d55
Cab38-09 <Cab38-09@03-C38-09>
2023-02-28 12:41:23 +0400

    Добавьте файлы проекта.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void TestBTN_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = new Button();
+            btn.Content = "Press me";
+            btn.Width = 75;
+            btn.Height = 50;
+            btn.Margin = new Thickness(100, 50, 0, 0); // расположение элемента в контейнере задается с помощью свойства Margin и объекта Thickness
+            btn.HorizontalAlignment = HorizontalAlignment.Left;
+            btn.VerticalAlignment = VerticalAlignment.Top;
+            rootwindow.Children.Add(btn);
+        }
+    }
+}
